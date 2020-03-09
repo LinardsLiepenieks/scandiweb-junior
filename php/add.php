36482f722cbc8dc4115bcd2ffd10177572b0afbd
LinardsLiepenieks <49512889+LinardsLiepenieks@users.noreply.github.com>
2020-03-10 00:18:44 +0200

    tjip product_add gatavs

--- php/add.php
+++ php/add.php
@@ -6,30 +6,55 @@
     $username = "root";
     $password = "";
     $dbname = "products";
-$conn = new mysqli($servername, $username, $password, $dbname);
+$conn = new mysqli($servername, $username, $password, $dbname) or die("Connection failed: " . $conn->connect_error);
+   
+
+//for testing reasons it creates database if not present, so that we dont need to manually create it
+if ($conn->connect_error){
+    
+    $conn = new mysqli($servername, $username, $password);
+    $q = mysqli_query($conn, 'SELECT * from products');
+ 
+    if($q == FALSE)
+    {
+        mysqli_query($conn, 'CREATE DATABASE products');
+        mysqli_query($conn, 'USE products');
+        mysqli_query($conn, 'CREATE TABLE items (SKU CHAR(10) PRIMARY KEY, name CHAR(30), price FLOAT, SA CHAR(40), weight FLOAT, height FLOAT, width FLOAT, length FLOAT, size FLOAT)');
+    }}
 
     $SKU = $_POST['sku'];
     $Name = $_POST['name'];
     $Price = $_POST['price'];
+    $Weight = $Height = $Length = $Width = $Size = '';
+    
     
 
+
     if($_POST['weight']!=="")
-        $SA = $_POST['weight'] . " KG";
+    {
+        $SA = "Book";
+        $Weight = $_POST['weight'];
+    }
 
     else if($_POST['height']!=="")
-        $SA = $_POST['height'] . "x" . $_POST['width'] . "x" . $_POST['length'];
+    {
+        $SA = "Furniture";
+        $Height = $_POST['height'];
+        $Width = $_POST['width'];
+        $Length = $_POST['length'];
+    }
 
-    else if ($_POST['size']!=="")
-        $SA = $_POST['size'] . " MB";
+    else if ($_POST['size']!==""){
+        $SA = "DVD-Disc";
+        $Size = $_POST['size'];}
 
     else{
         echo "Select a product type";
         return false;
     }
 
-
-    $sql = "INSERT INTO items (SKU, name, price, SA)
-    VALUES ('$SKU', '$Name', '$Price', '$SA')";
+    $sql = "INSERT INTO items (SKU, name, price, SA, weight, height, width, length, size)
+    VALUES ('$SKU', '$Name', '$Price', '$SA', '$Weight', '$Height', '$Width', '$Length', '$Size')";
 
     mysqli_query($conn, $sql);
 
